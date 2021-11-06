61bbf00b586ebea1a92541cae1848e20a656820e
lurbano <lurbano@earthsciweb.org>
2021-11-05 23:21:16 -0500

    functions

---
+++ logic/logic-eg.py
@@ -0,0 +1,11 @@
+x  = 10
+if x < 10:
+  print("x is less than 10.")
+elif x == 10:
+  print("x is equal to 10.")
+else:
+  print("x is greater than or equal to 10")
+
+x = 7
+if (x > 0) and (x < 10):
+    print("x is between 0 and 10.")
