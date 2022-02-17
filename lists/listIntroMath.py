908045b6b15e95660532790102833ddc905f1858
lurbano <lurbano@earthsciweb.org>
2022-02-17 16:01:29 -0600

    lists (WIP)

---
+++ lists/listIntroMath.py
@@ -0,0 +1,7 @@
+students = ['Brooke', 'Coen', 'Blas', 'George']
+exam1 = [85, 90, 80, 50]
+exam2 = [ 95, 100, 100, 96]
+
+print(students)
+# print out Blas' name and his grade for exam 1
+print(students[2], exam1[2])
