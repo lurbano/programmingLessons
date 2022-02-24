bb2e3ee821270f44623b0a7705d102833544fdd7
lurbano <lurbano@earthsciweb.org>
2022-02-24 16:47:50 -0600

    lists (Math)

--- lists/listIntroMath.py
+++ lists/listIntroMath.py
@@ -5,3 +5,31 @@ exam2 = [ 95, 100, 100, 96]
 print(students)
 # print out Blas' name and his grade for exam 1
 print(students[2], exam1[2])
+
+
+students = ['Brooke', 'Coen', 'Blas', 'George']
+exam1 = [85, 90, 80, 50]
+exam2 = [ 95, 100, 100, 96]
+print(students[2], exam1[2])
+
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+print(days[1], "and", days[5])
+
+nums = [2, 4, 6, 9, 10, 12, 14, 16, 18, 20]
+print(nums[-1])
+
+students = ['Brooke', 'Coen', 'Blas', 'George']
+print('students[0]:', students[0])
+print('students[1]:', students[1])
+print('students[-1]:', students[-1])
+print('students[-2]:', students[-2])
+print('students[1:3]:', students[1:3])
+print('students[:2]:', students[:2])
+print('students[1:]:', students[1:])
+
+students = ['Brooke', 'Coen', 'Blas', 'George']
+print(students[2], students[-2])
+
+exam1 = [85, 90, 80, 50]
+print(exam1[2:])
