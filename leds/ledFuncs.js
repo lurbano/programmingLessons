a4d5d0ced46d2b0e49c7dc4638852cfefc482f51
lurbano <lurbano@earthsciweb.org>
2022-04-09 21:44:24 -0500

    functions (wip)

--- leds/ledFuncs.js
+++ leds/ledFuncs.js
@@ -16,6 +16,24 @@ function lightSequence(leds, {
   return(endTime);
 }
 
+function lightBackwards(leds, {
+                        n=20,
+                        color="red",
+                        dt=0.1,
+                        startTime=0,
+                        step=1,
+                        pauseAfter=0.25
+                      } = {}) {
+
+  for (let i=0; i<n; i+=step){
+    setTimeout( function() {
+      leds[i].style.backgroundColor = color;
+    }, (startTime + (n-1-i)*dt)*1000);
+  }
+  let endTime = startTime + n *dt + pauseAfter;
+  return(endTime);
+}
+
 function clearLeds(leds, {
                             startTime=0,
                             n = 20,
